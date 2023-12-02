b8a80c723eccee2b6e20cefa6cd0ebdd25d956a6
Up <upcraftedlp@gmail.com>
2023-12-02 20:14:38 +0100

    2023 day 01

---
+++ src/day1.rs
@@ -0,0 +1,72 @@
+use crate::util::read_inputs;
+use anyhow::Result;
+
+pub fn run() -> Result<()> {
+    let inputs = read_inputs(1)?;
+    let mut result = 0;
+    for line in &inputs {
+        let mut chars: Vec<char> = Vec::new();
+        for c in line.chars().into_iter() {
+            if c.is_ascii_digit() {
+                chars.push(c);
+            }
+        }
+
+        let first = chars.first().unwrap();
+        let last = chars.last().unwrap();
+
+        let num: i32 = format!("{first}{last}").parse()?;
+        result += num;
+    }
+    println!("Day 1 Part 1: {result}");
+
+    // ---------------------------------------
+
+    result = 0;
+
+    let words = vec![
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    for line in &inputs {
+        let window_size = words.iter().map(|w| w.len()).max().unwrap();
+
+        let mut chars: Vec<char> = Vec::new();
+
+        'window: for idx in 0..line.len() {
+            let first_char = line.chars().skip(idx).next().unwrap();
+            if first_char.is_ascii_digit() {
+                chars.push(first_char);
+                continue 'window;
+            }
+
+            let current_window_size = std::cmp::min(window_size, line.len() - idx);
+            let window = &line[idx..idx + current_window_size];
+
+            for (w_idx, word) in words.iter().enumerate().map(|(i, w)| (i, *w)) {
+                if window.starts_with(word) {
+                    chars.push(std::char::from_digit((w_idx + 1) as u32, 10).unwrap());
+                    continue 'window;
+                }
+            }
+        }
+
+        let first = chars.first().unwrap();
+        let last = chars.last().unwrap();
+
+        let num: i32 = format!("{first}{last}").parse()?;
+        result += num;
+    }
+
+    println!("Day 1 Part 2: {result}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod util;
+mod day1;
 
 use anyhow::Result;
 
 fn main() -> Result<()> {
-
+    day1::run()?;
     Ok(())
 }
