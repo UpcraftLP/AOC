5246ec91d15a87e9a4e0b95ed255c21dd2f96cff
Up <upcraftedlp@gmail.com>
2024-12-01 06:12:04 +0100

    reset for 2024

--- src/day1.rs
+++ src/day1.rs
@@ -4,62 +4,11 @@ use anyhow::Result;
 pub(crate) fn run() -> Result<()> {
     let inputs = read_inputs(1)?;
     let mut result: i32 = 0;
-    for line in &inputs {
-        let chars: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
-
-        let first = chars.first().unwrap();
-        let last = chars.last().unwrap();
-
-        let num: i32 = format!("{first}{last}").parse()?;
-        result += num;
-    }
 
     println!("Day 1 Part 1: {result}");
 
     // ---------------------------------------
     result = 0;
-
-    let words = vec![
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-    ];
-
-    for line in &inputs {
-        let window_size = words.iter().map(|w| w.len()).max().unwrap();
-
-        let mut chars: Vec<char> = Vec::new();
-
-        'window: for idx in 0..line.len() {
-            let first_char = line.chars().skip(idx).next().unwrap();
-            if first_char.is_ascii_digit() {
-                chars.push(first_char);
-                continue 'window;
-            }
-
-            let current_window_size = std::cmp::min(window_size, line.len() - idx);
-            let window = &line[idx..idx + current_window_size];
-
-            for (w_idx, word) in words.iter().enumerate().map(|(i, w)| (i, *w)) {
-                if window.starts_with(word) {
-                    chars.push(std::char::from_digit((w_idx + 1) as u32, 10).unwrap());
-                    continue 'window;
-                }
-            }
-        }
-
-        let first = chars.first().unwrap();
-        let last = chars.last().unwrap();
-
-        let num: i32 = format!("{first}{last}").parse()?;
-        result += num;
-    }
 
     println!("Day 1 Part 2: {result}");
 
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,8 @@ use anyhow::Result;
 
 mod util;
 mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
-mod day10;
-mod day11;
-mod day12;
 
 fn main() -> Result<()> {
     day1::run()?;
-    day2::run()?;
-    day3::run()?;
-    day4::run()?;
-    day5::run()?;
-    day6::run()?;
-    day7::run()?;
-    day8::run()?;
-    day9::run()?;
-    day10::run()?;
-    day11::run()?;
-    day12::run()?;
     Ok(())
 }
