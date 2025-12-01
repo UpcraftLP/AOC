d1a1b62eae015da3b338600028bc5ffcc44a8f32
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-12-01 07:48:43 +0100

    reset for 2025

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,8 @@ use anyhow::Result;
 mod util;
 //---------
 mod day1;
-mod day10;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 fn main() -> Result<()> {
     day1::run()?;
-    day2::run()?;
-    day3::run()?;
-    day4::run()?;
-    day5::run()?;
-    day6::run()?;
-    day7::run()?;
-    day8::run()?;
-    day9::run()?;
-    day10::run()?;
     Ok(())
 }
