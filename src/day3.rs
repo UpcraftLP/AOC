9339087f79289c65d01869e970c0df808484cf53
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-12-03 08:07:24 +0100

    2025 day 03

---
+++ src/day3.rs
@@ -0,0 +1,64 @@
+use crate::util::read_inputs;
+use anyhow::Result;
+use itertools::Itertools;
+
+pub(crate) fn run() -> Result<()> {
+    let inputs = read_inputs(3)?;
+
+    let banks = inputs
+        .iter()
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_digit(10).expect("expected digit"))
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let result: u64 = banks
+        .iter()
+        .map(|bank| {
+            let (_, joltage) = (0..2).rev().fold((0usize, 0u64), |acc, i| {
+                let max = bank
+                    .iter()
+                    .enumerate()
+                    .rev()
+                    .take(bank.len() - acc.0)
+                    .skip(i)
+                    .max_by_key(|(_, a)| *a)
+                    .expect("no max");
+
+                (max.0 + 1, acc.1 * 10 + *max.1 as u64)
+            });
+
+            joltage
+        })
+        .sum();
+
+    println!("Day 2 Part 1: {result}");
+
+    // ---------------------------------------
+
+    let result: u64 = banks
+        .iter()
+        .map(|bank| {
+            let (_, joltage) = (0..12).rev().fold((0usize, 0u64), |acc, i| {
+                let max = bank
+                    .iter()
+                    .enumerate()
+                    .rev()
+                    .take(bank.len() - acc.0)
+                    .skip(i)
+                    .max_by_key(|(_, a)| *a)
+                    .expect("no max");
+
+                (max.0 + 1, acc.1 * 10 + *max.1 as u64)
+            });
+
+            joltage
+        })
+        .sum();
+
+    println!("Day 2 Part 2: {result}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod util;
 //---------
 mod day1;
 mod day2;
+mod day3;
 
 fn main() -> Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     day1::run()?;
     day2::run()?;
+    day3::run()?;
     Ok(())
 }
