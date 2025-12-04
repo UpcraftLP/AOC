21d02d3a73c951a09243ebb3c5974dfe44942d72
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-12-04 08:18:12 +0100

    print file reading to debug log

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 
 pub fn read_inputs(day: i8) -> Result<Vec<String>> {
     let file_name = format!("day_{day:02}.txt");
-    println!("Reading {}", file_name);
+    log::debug!("Reading {}", file_name);
     let content = fs::read_to_string(format!("./input/{file_name}"))?;
     let lines = content
         .lines()
